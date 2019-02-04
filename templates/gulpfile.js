a3c96636353f4eb22d804b5161f4dcc5f8065426
Elf-mousE <ifmouz@gmail.com>
2019-02-04 12:06:10 +0800

    balm 1.0.0 released

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,6 +1,6 @@
 // Documentation - http://balmjs.com/docs/en/configuration/toc.html
 // 中文文档 - http://balmjs.com/docs/zh-cn/configuration/toc.html
-var balm = require('balm');
+const balm = require('balm');
 
 balm.config = {
   roots: {
